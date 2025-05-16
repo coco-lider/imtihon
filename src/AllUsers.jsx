295b9d346391d67b5bc6ff3c96dbb44617b3c5ba
coco-lider <umidtuxtayev84@gmail.com>
2025-05-16 17:48:47 +0500

    first commit

--- src/AllUsers.jsx
+++ src/AllUsers.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
 const AllUsers = () => {
@@ -28,11 +28,41 @@ const AllUsers = () => {
       controller.abort();
     };
   }, []);
+
+  let inputValue = useRef(null)
+  let login = useRef(null)
+  let p = useRef(null)
+
+  function handleLogin() {
+    login.current.style.display = 'flex'
+    p.current.style.display = 'none'
+
+  }
+
+  function handleClose() {
+    login.current.style.display = 'none'
+
+  }
+
+  function handleWelcome() {
+    setName(inputValue.current.value)
+    login.current.style.display = 'none'
+    p.current.style.display = 'block'
+  }
+
   return (
     <div>
       <div>
         <h1>User Manager</h1>
-        <button>Login</button>
+        <div className="border-b p-5">
+          <button onClick={handleLogin} className="  mb-5 border p-2 rounded">Login</button>
+          <div ref={login} className="flex-col gap-5 none hidden">
+            <button onClick={handleClose} className="p-2 border rounded w-20">close</button>
+            <input ref={inputValue} className="border p-2 rounded w-50" type="text" placeholder="name" />
+            <button onClick={handleWelcome} className="p-2 border rounded w-20">Submit</button>
+          </div>
+          <p ref={p} className="text-2xl hidden ">Welcome  website <b>{name}</b></p>
+        </div >
       </div>
       <div>
         <h1>Users</h1>
